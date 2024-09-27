e437cc73d0a5abdde5bd3f62cd69c84ac883d64d
Hailey Kim <hailey@Haileys-Laptop.local>
2024-09-27 11:26:19 +0900

    fix: update requests(routing, :root, etc)

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,6 +7,8 @@ import ComparisonResultPage from "./pages/ComparisonResultPage.jsx";
 import ComparisonStatusPage from "./pages/ComparisonStatusPage.jsx";
 import InvestmentStatusPage from "./pages/InvestmentStatusPage.jsx";
 import MyComparisonPage from "./pages/MyComparisonPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function Main() {
@@ -15,15 +17,25 @@ function Main() {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<LandingPage />} />
-          <Route path="companies">
-            <Route index element={<CompanyListPage />} />
-            <Route path=":companyId" element={<CompanyDetailPage />} />
-          </Route>
-          <Route path="comparison-status" element={<ComparisonStatusPage />} />
-          <Route path="investment-status" element={<InvestmentStatusPage />} />
-          <Route path="my-comparison">
-            <Route index element={<MyComparisonPage />} />
-            <Route path="result" element={<ComparisonResultPage />} />
+          <Route path="login" element={LoginPage} />
+          <Route path="signup" element={SignupPage} />
+          <Route path="user/:userId">
+            <Route path="companies">
+              <Route index element={<CompanyListPage />} />
+              <Route path=":companyId" element={<CompanyDetailPage />} />
+            </Route>
+            <Route
+              path="comparison-status"
+              element={<ComparisonStatusPage />}
+            />
+            <Route
+              path="investment-status"
+              element={<InvestmentStatusPage />}
+            />
+            <Route path="my-comparison">
+              <Route index element={<MyComparisonPage />} />
+              <Route path="result" element={<ComparisonResultPage />} />
+            </Route>
           </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Route>
