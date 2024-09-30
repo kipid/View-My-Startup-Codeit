b994522a7a29ce7ba739f1fe9a3b179e71f8a6f4
오성현 <henryosh@oseonghyeon-ui-MacBookPro.local>
2024-10-01 08:28:04 +0900

    chore: detailed design, table width problem 발생 헤더 테이블과 바디테이블 간격이 서로 맞지 않습니다. 이 문제 해결해야함.

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -4,32 +4,38 @@ import styles from './CompanyList.module.css'; // 스타일 파일을 따로 생
 const CompanyList = ({ companies }) => {
   return (
     <div className={styles.companyList}>
-      <table>
-        <thead>
-          <tr>
-            <th>순위</th>
-            <th>기업 명</th>
-            <th>기업 소개</th>
-            <th>카테고리</th>
-            <th>누적 투자 금액</th>
-            <th>매출액</th>
-            <th>고용 인원</th>
-          </tr>
-        </thead>
-        <tbody>
-          {companies.map((company, index) => (
-            <tr key={company.id}>
-              <td>{index + 1}위</td>
-              <td>{company.name}</td>
-              <td>{company.description}</td>
-              <td>{company.category}</td>
-              <td>{company.cumulativeInvestmentAmount} 원</td>
-              <td>{company.renenue} 원</td>
-              <td>{company.employees} 명</td>
+      <div className={styles.headerTableContainer}>
+        <table className={styles.headerTable}>
+          <thead>
+            <tr>
+              <th>순위</th>
+              <th>기업 명</th>
+              <th>기업 소개</th>
+              <th>카테고리</th>
+              <th>누적 투자 금액</th>
+              <th>매출액</th>
+              <th>고용 인원</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+        </table>
+      </div>
+      <div className={styles.bodyTableContainer}>
+        <table className={styles.bodyTable}>
+          <tbody>
+            {companies.map((company, index) => (
+              <tr key={company.id}>
+                <td>{index + 1}위</td>
+                <td>{company.name}</td>
+                <td>{company.description}</td>
+                <td>{company.category}</td>
+                <td>{company.cumulativeInvestmentAmount} 원</td>
+                <td>{company.renenue} 원</td>
+                <td>{company.employees} 명</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
