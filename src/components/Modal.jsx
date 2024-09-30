73ba136992b1f907578811fccbce23528374891f
Maybeiley <2784519@gmail.com>
2024-10-01 08:53:12 +0900

    feat: add common modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import styles from "./Modal.module.css";
+
+function Modal({ children }) {
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modalWrapper}>
+        <div className={styles.modalContents}>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
