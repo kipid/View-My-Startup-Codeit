b6efcf802de05114759f09546dc66da51b3ac5f3
kipid <kipacti@gmail.com>
2024-09-30 16:02:27 +0900

    Feat: CompanyListPage

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,32 +5,32 @@ let pageName = process.argv[2];
 let dirName = `${pageName}/`;
 
 if (!pageName) {
-  console.log('Default is index[.js]');
-  pageName = 'index';
-  dirName = '';
+	console.log('Default is index[.js]');
+	pageName = 'index';
+	dirName = '';
 }
 
 esbuild.build({
-  sourcemap: true,
-  logLevel: 'info',
-  platform: 'browser',
-  entryPoints: [`src/${pageName}.js`],
-  bundle: true,
-  minify: false,
-  outfile: `dist/${dirName}esb-bundle.js`,
-  write: true,
-  loader: {
-    '.js': 'jsx',
-    '.svg': 'file',
-    '.jpg': 'file',
-    '.jpeg': 'file',
-    '.png': 'file',
-  },
-  jsx: 'automatic',
-  define: { 'process.env.NODE_ENV': '"production"' },
-  jsxFactory: 'React.createElement',
-  jsxFragment: 'React.Fragment',
-  inject: ['./react-shim.js'], // 자동 주입 파일 추가
-  // Other options...
+	sourcemap: true,
+	logLevel: 'info',
+	platform: 'browser',
+	entryPoints: [`src/${pageName}.jsx`],
+	bundle: true,
+	minify: false,
+	outfile: `dist/${dirName}esb-bundle.js`,
+	write: true,
+	loader: {
+		'.js': 'jsx',
+		'.svg': 'file',
+		'.jpg': 'file',
+		'.jpeg': 'file',
+		'.png': 'file',
+	},
+	jsx: 'automatic',
+	define: { 'process.env.NODE_ENV': '"production"' },
+	jsxFactory: 'React.createElement',
+	jsxFragment: 'React.Fragment',
+	inject: ['./react-shim.js'], // 자동 주입 파일 추가
+	treeShaking: true,
 })
 .catch(() => process.exit(1));
