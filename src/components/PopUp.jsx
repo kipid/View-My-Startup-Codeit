5bcdb72f08e713631cd23e06e88826038c7d8438
kipid <kipacti@gmail.com>
2024-10-01 17:08:20 +0900

    Update PopUp.jsx

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -6,13 +6,8 @@ function PopUp({ error, popUpText, setError }) {
 			<div className={styles.popUpContainer}>
 				<div className={styles.popUpText}>{popUpText}</div>
 				<button
-<<<<<<< HEAD
-					className={styles.popUpCloseButton}
-					type="button"
-=======
 					type="button"
 					className={styles.popUpCloseButton}
->>>>>>> dev
 					onClick={() => {
 						setError(null);
 					}}
