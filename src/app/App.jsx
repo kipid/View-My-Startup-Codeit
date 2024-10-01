8d74bae3555c40e410432f741092bba550ea7c2b
kipid <kipacti@gmail.com>
2024-10-01 17:33:44 +0900

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import GNB from '../components/GNB.jsx';
-import styles from './components/Pagination.module.css';
+import styles from '../components/Pagination.module.css';
 
 function App() {
 	return (
