dd1a86f3c73340147c3181f4eb4b60527ca6db5d
kipid <kipacti@gmail.com>
2024-10-01 17:32:47 +0900

    Update companiesService.js

--- src/apis/companiesService.js
+++ src/apis/companiesService.js
@@ -1,20 +1,11 @@
 import instance from './instance.js';
 
 export async function getCompanies(params = { skip: 0, take: 20, sort: 'recent', keyword: '' }) {
-	// try {
 	const companies = await instance.get(`/companies`, { params });
 	return companies.data;
-	// }
-	// catch (err) {
-	// 	return err?.response?.data || err;
-	// }
 }
 
 export async function getCompanyWithId(id) {
-	try {
-		const company = await instance.get(`/companies/${id}`);
-		return company.data;
-	} catch (err) {
-		return err?.response?.data || err;
-	}
+	const company = await instance.get(`/companies/${id}`);
+	return company.data;
 }
