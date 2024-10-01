a0c790ac5b2d7c77f5e9bdbc70fd6f723255d35b
kipid <kipacti@gmail.com>
2024-10-02 05:57:08 +0900

    Update Main.jsx

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import App from './App.jsx';
 import LandingPage from '../pages/LandingPage.jsx';
 import CompanyDetailPage from '../pages/CompanyDetailPage.jsx';
@@ -10,31 +11,47 @@ import MyComparisonPage from '../pages/MyComparisonPage.jsx';
 import LoginPage from '../pages/LoginPage.jsx';
 import SignupPage from '../pages/SignupPage.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
+import { UserProvider, useSetUser } from '../context/UserProvider.jsx';
 
 function Main() {
+	const setUser = useSetUser();
+	useEffect(() => {
+		const userUuid = localStorage.getItem('userUuid');
+		if (userUuid) {
+			const nickname = localStorage.getItem('nickname');
+			const sessionSalt = localStorage.getItem('sessionSalt');
+			const sessionPwd = localStorage.getItem('sessionPwd');
+			if (sessionSalt && sessionPwd) {
+				setUser({ uuid: userUuid, nickname, sessionSalt, sessionPwd });
+			}
+		}
+	}, [setUser]);
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />}>
-					<Route index element={<LandingPage />} />
-					<Route path="login" element={<LoginPage />} />
-					<Route path="signup" element={<SignupPage />} />
-					<Route path="user/:userId">
-						<Route path="companies">
-							<Route index element={<CompanyListPage />} />
-							<Route path=":companyId" element={<CompanyDetailPage />} />
+		<UserProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />}>
+						<Route index element={<LandingPage />} />
+						<Route path="login" element={<LoginPage />} />
+						<Route path="signup" element={<SignupPage />} />
+						<Route path="user/:userId">
+							<Route path="companies">
+								<Route index element={<CompanyListPage />} />
+								<Route path=":companyId" element={<CompanyDetailPage />} />
+							</Route>
+							<Route path="comparison-status" element={<ComparisonStatusPage />} />
+							<Route path="investment-status" element={<InvestmentStatusPage />} />
+							<Route path="my-comparison">
+								<Route index element={<MyComparisonPage />} />
+								<Route path="result" element={<ComparisonResultPage />} />
+							</Route>
 						</Route>
-						<Route path="comparison-status" element={<ComparisonStatusPage />} />
-						<Route path="investment-status" element={<InvestmentStatusPage />} />
-						<Route path="my-comparison">
-							<Route index element={<MyComparisonPage />} />
-							<Route path="result" element={<ComparisonResultPage />} />
-						</Route>
+						<Route path="*" element={<NotFoundPage />} />
 					</Route>
-					<Route path="*" element={<NotFoundPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
 	);
 }
 
