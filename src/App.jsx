1f534151bc0f7df1ad1ba430f9154c2f084dece0
kipid <kipacti@gmail.com>
2024-10-01 14:27:12 +0900

    Update App.jsx
    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import GNB from "./components/GNB";
-import { Outlet } from "react-router-dom";
-import styles from "./components/Pagination.module.css";
+import { Outlet } from 'react-router-dom';
+import GNB from './components/GNB';
+import styles from './components/Pagination.module.css';
 
 function App() {
 	return (
