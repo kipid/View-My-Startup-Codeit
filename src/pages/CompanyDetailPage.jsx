1b2222db148d9c6e17b481ad844a593d5706dd1e
csbizz <95736373+csbizz@users.noreply.github.com>
2024-10-01 13:18:30 +0900

    Fix: Table layout & Pagination

--- src/pages/CompanyDetailPage.jsx
+++ src/pages/CompanyDetailPage.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import Pagination from '../components/Pagination.jsx';
+
 function CompanyDetailPage() {
+	const [pageNum, setPageNum] = useState(1);
+	const [pageNumMax, setPageNumMax] = useState(1);
+
 	return (
 		<div id="companyDetailPage" style={{ width: 1200 }}>
 			<div id="investments" style={{ width: '100%', height: 583, display: 'flex', flexDirection: 'column', gap: 24 }}>
@@ -27,46 +33,50 @@ function CompanyDetailPage() {
 
 				<div id="investmentBody" style={{ display: 'flex', flexDirection: 'column' }}>
 					<table style={{ width: '100%', height: 375, paddingTop: 16 }}>
-						<thead>총 X원</thead>
-						<tr className="table-margin" style={{ height: 16 }} />
-						<tr height="39px">
-							<th width="84px">투자자 이름</th>
-							<th width="84px">순위</th>
-							<th width="84px">투자 금액</th>
-							<th>투자 코멘트</th>
-						</tr>
-						<tr className="table-margin" style={{ height: 16 }} />
-						<tr style={{ textAlign: 'center' }}>
-							<td>김연우</td>
-							<td>1위</td>
-							<td>10억</td>
-							<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
-						</tr>
-						<tr style={{ textAlign: 'center' }}>
-							<td>김연우</td>
-							<td>1위</td>
-							<td>10억</td>
-							<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
-						</tr>
-						<tr style={{ textAlign: 'center' }}>
-							<td>김연우</td>
-							<td>1위</td>
-							<td>10억</td>
-							<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
-						</tr>
-						<tr style={{ textAlign: 'center' }}>
-							<td>김연우</td>
-							<td>1위</td>
-							<td>10억</td>
-							<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
-						</tr>
-						<tr style={{ textAlign: 'center' }}>
-							<td>김연우</td>
-							<td>1위</td>
-							<td>10억</td>
-							<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
-						</tr>
+						<caption style={{ textAlign: 'left' }}>총 X원</caption>
+						<thead>
+							<tr height="39px">
+								<th width="84px">투자자 이름</th>
+								<th width="84px">순위</th>
+								<th width="84px">투자 금액</th>
+								<th>투자 코멘트</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr style={{ textAlign: 'center', height: 64 }}>
+								<td>김연우</td>
+								<td>1위</td>
+								<td>10억</td>
+								<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
+							</tr>
+							<tr style={{ textAlign: 'center', height: 64 }}>
+								<td>김연우</td>
+								<td>1위</td>
+								<td>10억</td>
+								<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
+							</tr>
+							<tr style={{ textAlign: 'center', height: 64 }}>
+								<td>김연우</td>
+								<td>1위</td>
+								<td>10억</td>
+								<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
+							</tr>
+							<tr style={{ textAlign: 'center', height: 64 }}>
+								<td>김연우</td>
+								<td>1위</td>
+								<td>10억</td>
+								<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
+							</tr>
+							<tr style={{ textAlign: 'center', height: 64 }}>
+								<td>김연우</td>
+								<td>1위</td>
+								<td>10억</td>
+								<td style={{ textAlign: 'left' }}>코드잇은 정말 훌륭한 기업입니다!</td>
+							</tr>
+						</tbody>
 					</table>
+
+					<Pagination pageNum={pageNum} pageNumMax={pageNumMax} setPageNum={setPageNum} />
 				</div>
 			</div>
 		</div>
