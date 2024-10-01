434f32d8480dd40ec7d148fe84b76f93b937464a
kipid <kipacti@gmail.com>
2024-10-01 14:27:05 +0900

    Update companiesService.js
    formatting

--- src/apis/companiesService.js
+++ src/apis/companiesService.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `https://localhost:3000`
+	baseURL: `https://localhost:3000`,
 });
 
-export async function getCompanies(params = { skip: 0, take: 20, sort: "recent", keyword: "" }) {
+export async function getCompanies(params = { skip: 0, take: 20, sort: 'recent', keyword: '' }) {
 	// try {
-		const companies = await instance.get(`/companies`, {params});
-		return companies.data;
+	const companies = await instance.get(`/companies`, { params });
+	return companies.data;
 	// }
 	// catch (err) {
 	// 	return err?.response?.data || err;
@@ -18,8 +18,7 @@ export async function getCompanyWithId(id) {
 	try {
 		const company = await instance.get(`/companies/${id}`);
 		return company.data;
-	}
-	catch (err) {
+	} catch (err) {
 		return err?.response?.data || err;
 	}
 }
