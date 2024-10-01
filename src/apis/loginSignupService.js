ddd1bb0fb98b694e504ba2356ad0f6cf11a00dec
kipid <kipacti@gmail.com>
2024-10-01 16:01:20 +0900

    Update loginSignupService.js

---
+++ src/apis/loginSignupService.js
@@ -0,0 +1,18 @@
+import instance from './instance.js';
+
+export async function postPwdIter(data = { email: '' }) {
+	const iter = await instance.post(`/account/iter`, data);
+	return iter.data;
+}
+
+export async function postLogin(data = { email: 'anonymous@example.com', pwdEncrypted: '123456' }) {
+	const user = await instance.post(`/account/log-in`, data);
+	return user.data;
+}
+
+export async function postSignup(
+	data = { email: 'anonymous@example.com', nickname: 'anonymous', pwdEncrypted: '123456', pwdConfirm: 'confirmed' },
+) {
+	const user = await instance.post(`/account/sign-up`, data);
+	return user.data;
+}
