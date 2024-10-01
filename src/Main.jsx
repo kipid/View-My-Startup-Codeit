3b553bbcde31eb079df43843c5523c09fec9cadc
kipid <kipacti@gmail.com>
2024-10-01 14:27:26 +0900

    Update Main.jsx
    formatting

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import LandingPage from "./pages/LandingPage.jsx";
-import CompanyDetailPage from "./pages/CompanyDetailPage.jsx";
-import CompanyListPage from "./pages/CompanyListPage.jsx";
-import ComparisonResultPage from "./pages/ComparisonResultPage.jsx";
-import ComparisonStatusPage from "./pages/ComparisonStatusPage.jsx";
-import InvestmentStatusPage from "./pages/InvestmentStatusPage.jsx";
-import MyComparisonPage from "./pages/MyComparisonPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import SignupPage from "./pages/SignupPage.jsx";
-import NotFoundPage from "./pages/NotFoundPage.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import LandingPage from './pages/LandingPage.jsx';
+import CompanyDetailPage from './pages/CompanyDetailPage.jsx';
+import CompanyListPage from './pages/CompanyListPage.jsx';
+import ComparisonResultPage from './pages/ComparisonResultPage.jsx';
+import ComparisonStatusPage from './pages/ComparisonStatusPage.jsx';
+import InvestmentStatusPage from './pages/InvestmentStatusPage.jsx';
+import MyComparisonPage from './pages/MyComparisonPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 function Main() {
 	return (
@@ -24,14 +24,8 @@ function Main() {
 							<Route index element={<CompanyListPage />} />
 							<Route path=":companyId" element={<CompanyDetailPage />} />
 						</Route>
-						<Route
-							path="comparison-status"
-							element={<ComparisonStatusPage />}
-						/>
-						<Route
-							path="investment-status"
-							element={<InvestmentStatusPage />}
-						/>
+						<Route path="comparison-status" element={<ComparisonStatusPage />} />
+						<Route path="investment-status" element={<InvestmentStatusPage />} />
 						<Route path="my-comparison">
 							<Route index element={<MyComparisonPage />} />
 							<Route path="result" element={<ComparisonResultPage />} />
