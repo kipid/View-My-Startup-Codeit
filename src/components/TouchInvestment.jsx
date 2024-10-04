6bd11b30a328d0b2936986d7f785ba5746bce8a4
csbizz <95736373+csbizz@users.noreply.github.com>
2024-10-04 10:11:08 +0900

    Feat: Add listener for dropdown off

--- src/components/TouchInvestment.jsx
+++ src/components/TouchInvestment.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import kebab from '../assets/ic_kebab.png';
 import style from './TouchInvestment.module.css';
 
 function TouchInvestment({ investmentDetail, onUpdate, onDelete }) {
+	const dropdownRef = useRef();
 	const [dropdownOpen, setDropdownOpen] = useState(false);
 
 	const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
@@ -15,8 +16,18 @@ function TouchInvestment({ investmentDetail, onUpdate, onDelete }) {
 		setDropdownOpen(false);
 	};
 
+	useEffect(() => {
+		const handleClickOutside = e => {
+			// NOTE Ref의 current에 담긴 엘리먼트가 아닌 곳(바깥)을 클릭 시 드롭다운 메뉴 닫힘
+			if (dropdownOpen && !dropdownRef.current.contains(e.target)) setDropdownOpen(false);
+		};
+		document.addEventListener('click', handleClickOutside);
+
+		return () => document.removeEventListener('click', handleClickOutside);
+	}, [dropdownOpen]);
+
 	return (
-		<div id={style.dropdownWrapper}>
+		<div id={style.dropdownWrapper} ref={dropdownRef}>
 			<button type="button" onClick={toggleDropdown}>
 				<img src={kebab} alt="kebab menu Icon" />
 			</button>
