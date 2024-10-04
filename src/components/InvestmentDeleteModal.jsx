78f535db286ff34cc3dcc330230ef43966d52352
csbizz <95736373+csbizz@users.noreply.github.com>
2024-10-04 09:53:52 +0900

    Feat: Add password show button

--- src/components/InvestmentDeleteModal.jsx
+++ src/components/InvestmentDeleteModal.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import style from './InvestmentModals.module.css';
 import icDelete from '../assets/ic_delete.png';
-import noImage from '../assets/no_image.png';
+import eyeOn from '../assets/ic_eye_on.png';
+import eyeOff from '../assets/ic_eye_off.png';
 
 function InvestmentDeleteModal({ investmentDetail, onClose, show = false }) {
+	const [isPWshow, setIsPWshow] = useState(false);
+
 	// NOTE show가 false이면 아무것도 렌더하지 않음
 	if (!show) return null;
+
+	const togglePWshow = () => {
+		setIsPWshow(!isPWshow);
+	};
 
 	return (
 		<div id={style.InvestmentDeleteModal}>
@@ -17,8 +25,9 @@ function InvestmentDeleteModal({ investmentDetail, onClose, show = false }) {
 
 			<form id={style.modalBody}>
 				<div id={style.password}>
-					<p>비밀번호</p>
-					<input type="password" placeholder="비밀번호를 입력해 주세요" />
+					<label htmlFor="password">비밀번호</label>
+					<input id="password" type={isPWshow ? '' : 'password'} placeholder="비밀번호를 입력해 주세요" />
+					<img id={style.eye} src={isPWshow ? eyeOn : eyeOff} alt="비밀번호 표시" onClick={togglePWshow} />
 				</div>
 			</form>
 
--- src/components/InvestmentUpdateModal.jsx
+++ src/components/InvestmentUpdateModal.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import style from './InvestmentModals.module.css';
 import icDelete from '../assets/ic_delete.png';
 import noImage from '../assets/no_image.png';
+import eyeOn from '../assets/ic_eye_on.png';
+import eyeOff from '../assets/ic_eye_off.png';
 
 function InvestmentUpdateModal({ investmentDetail, onClose, show = false }) {
+	const [isPWshow, setIsPWshow] = useState(false);
+
 	// NOTE show가 false이면 아무것도 렌더하지 않음
 	if (!show) return null;
 
-	const handleClose = () => {};
+	const togglePWshow = () => {
+		setIsPWshow(!isPWshow);
+	};
 
 	const logo = ''; // TODO investmentDetail로부터 coompanyId를 받아서 logo를 가져올것.
 
@@ -29,20 +36,21 @@ function InvestmentUpdateModal({ investmentDetail, onClose, show = false }) {
 					</div>
 				</div>
 				<div id={style.name}>
-					<p>투자자 이름</p>
-					<input type="text" placeholder="투자자 이름을 입력해 주세요" value={investmentDetail.name} />
+					<label htmlFor="name">투자자 이름</label>
+					<input id="name" type="text" placeholder="투자자 이름을 입력해 주세요" value={investmentDetail.name} />
 				</div>
 				<div id={style.amount}>
-					<p>투자 금액</p>
-					<input type="number" placeholder="투자 금액을 입력해 주세요" value={investmentDetail.amount} />
+					<label htmlFor="amount">투자 금액</label>
+					<input id="amount" type="number" placeholder="투자 금액을 입력해 주세요" value={investmentDetail.amount} />
 				</div>
 				<div id={style.comment}>
-					<p>투자 코멘트</p>
-					<textarea placeholder="투자에 대한 코멘트를 입력해 주세요" value={investmentDetail.comment} />
+					<label htmlFor="comment">투자 코멘트</label>
+					<textarea id="comment" placeholder="투자에 대한 코멘트를 입력해 주세요" value={investmentDetail.comment} />
 				</div>
 				<div id={style.password}>
-					<p>비밀번호</p>
-					<input type="password" placeholder="비밀번호를 입력해 주세요" />
+					<label htmlFor="password">비밀번호</label>
+					<input id="password" type={isPWshow ? '' : 'password'} placeholder="비밀번호를 입력해 주세요" />
+					<img id={style.eye} src={isPWshow ? eyeOn : eyeOff} alt="비밀번호 표시" onClick={togglePWshow} />
 				</div>
 			</form>
 
