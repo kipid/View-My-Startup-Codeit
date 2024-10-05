9dfb40101114bf9cb85c5223c0192aa1d06857dd
kipid <kipacti@gmail.com>
2024-10-06 08:17:41 +0900

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ import styles from './Modal.module.css';
 
 function Modal({ children }) {
 	return (
-		<div id={styles.modalContainer}>
+		<div id={styles.modalContainer} onScroll={e => e.stopPropagation()}>
 			<div id={styles.modalWrapper}>
 				<div id={styles.modalContents}>{children}</div>
 			</div>
