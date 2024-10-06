844e08c4a3866f654d34b14ea8bbe9ed8d79e7a4
kipid <kipacti@gmail.com>
2024-10-07 08:14:33 +0900

    Revert "Update investmentApis.js"
    This reverts commit 23757c7b4be91bac15025fb42bc4b9f90018f783.

--- src/shared/apis/investmentApis.js
+++ src/shared/apis/investmentApis.js
@@ -1,19 +1,19 @@
-import instance from './instance.js';
+import { axiosGet, axiosPatch } from '../utils/axiosUtils.js';
 
 export async function getInvestments(params = { page: 0, pageSize: 10, orderBy: 'bigger' }) {
-	const investments = await instance.get(`/investments`, { params });
+	const investments = await axiosGet(`/investments`, params);
 
 	return investments;
 }
 
 export async function getInvestmentsTotalAmount() {
-	const totalAmount = await instance.get(`/investments/total`);
+	const totalAmount = await axiosGet(`/investments/total`);
 
 	return totalAmount;
 }
 
 export async function updateInvestment(id, body) {
-	const investment = await instance.patch(`/investments/${id}`, body);
+	const investment = await axiosPatch(`/investments/${id}`, body);
 
 	return investment;
 }
