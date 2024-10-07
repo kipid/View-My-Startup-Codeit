ba904791f97b25d297508570c8669bc35b1c85bc
kipid <kipacti@gmail.com>
2024-10-07 17:16:22 +0900

    a link styles to CSS, not inline style

--- src/pages/ComparisonStatusPage.jsx
+++ src/pages/ComparisonStatusPage.jsx
@@ -77,7 +77,7 @@ function ComparisonStatusPage() {
 									<tr key={company.id}>
 										<td>{i + 1 + pageSize * (pageNum - 1)}위</td>
 										<td>
-											<Link to={`/companies/${company.id}`} style={{ textDecoration: 'none', color: 'var(--prime-blue)' }}>
+											<Link to={`/companies/${company.id}`}>
 												<img className={styles.logo} src={company.logo ? company.logo : noLogo} alt="Company Logo" />
 												&nbsp; {company.name}
 											</Link>
