4466ace3c34691a3d8f2286355619ba1e825d2a7
kipid <kipacti@gmail.com>
2024-10-08 13:15:21 +0900

    Feat: log-out-from-all

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,18 +2,18 @@ import { Link, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import styles from './LoginPage.module.css';
 import { useSetUser, useUser } from '../context/UserProvider.jsx';
-import { postLogout, postSsnIter } from '../shared/apis/loginSignupService.js';
-import encrypt from '../shared/apis/encrypt.js';
+import { postLogout, postSsnAndCallback, getSsns, postLogoutFromAll } from '../shared/apis/loginSignupService.js';
 import PopUp from '../components/PopUp.jsx';
 
 function Profile() {
+	const [sessions, setSessions] = useState([]);
 	const [error, setError] = useState(null);
 	const user = useUser();
 	const setUser = useSetUser();
 
 	return (
 		<>
-			{!user && <Navigate to="/login" />}
+			{!user && !error && <Navigate to="/login" />}
 			<section className={styles.section}>
 				<Link to="/">
 					<img className={styles.logo} src="/images/site-logo.png" alt="View My StartUp Logo" />
@@ -27,6 +27,35 @@ function Profile() {
 						<h3>닉네임</h3>
 						<p>{user?.nickname}</p>
 					</div>
+					<button
+						className={styles.getSsns}
+						type="button"
+						onClick={() => {
+							postSsnAndCallback({ userId: user.userUuid, createdAt: user.createdAt, sessionPwd: user.sessionPwd }, async () => {
+								setSessions(await getSsns());
+							});
+						}}
+					>
+						모든 Session 불러오기
+					</button>
+					<table className={styles.table}>
+						<tbody>
+							<tr>
+								<th>IP</th>
+								<th>Iteration</th>
+								<th>created at</th>
+							</tr>
+							{sessions.map(session => {
+								return (
+									<tr key={session.createdAt}>
+										<td>{session.ip}</td>
+										<td>{session.iter}</td>
+										<td>{session.createdAt}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
 					<div className={styles.profileElement}>
 						<h3>로그인 시간</h3>
 						<p>{user?.createdAt}</p>
@@ -38,17 +67,27 @@ function Profile() {
 					onClick={() => {
 						localStorage.clear();
 						setUser(null);
-						postSsnIter({ userId: user.userUuid, createdAt: user.createdAt }).then(async iter => {
-							console.log('user', user);
-							console.log('iter', iter);
-							const sessionEncrypted = encrypt(iter.sessionSalt, user.sessionPwd, iter.iter);
-							const result = await postLogout({ userId: user.userUuid, createdAt: user.createdAt, sessionEncrypted });
-							console.log('result', result);
+						postSsnAndCallback({ userId: user.userUuid, createdAt: user.createdAt, sessionPwd: user.sessionPwd }, async () => {
+							const result = await postLogout({ userId: user.userUuid, createdAt: user.createdAt });
 							setError(result);
 						});
 					}}
 				>
 					로그아웃
+				</button>
+				<button
+					className={styles.logout}
+					type="button"
+					onClick={() => {
+						localStorage.clear();
+						setUser(null);
+						postSsnAndCallback({ userId: user.userUuid, createdAt: user.createdAt, sessionPwd: user.sessionPwd }, async () => {
+							const result = await postLogoutFromAll({ userId: user.userUuid });
+							setError(result);
+						});
+					}}
+				>
+					모든 곳으로부터 로그아웃
 				</button>
 				<PopUp error={error} setError={setError} popUpText={error?.message} />
 			</section>
--- src/shared/apis/loginSignupService.js
+++ src/shared/apis/loginSignupService.js
@@ -1,3 +1,5 @@
+import docCookies from './cookies.js';
+import encrypt from './encrypt.js';
 import instance from './instance.js';
 
 export async function postPwdIter(data = { email: '' }) {
@@ -6,10 +8,26 @@ export async function postPwdIter(data = { email: '' }) {
 	// return { iter: Number, salt: String }
 }
 
-export async function postSsnIter(data = { userId: '', createdAt: 0 }) {
-	const iter = await instance.post(`/account/session-iter`, data);
+export async function getSsnIter(data = { userId: '', createdAt: 0 }) {
+	docCookies.setItem('userId', data.userId, 1, '/', null, true);
+	docCookies.setItem('createdAt', data.createdAt, 1, '/', null, true);
+	const iter = await instance.get(`/account/session-iter`, data);
 	return iter.data;
 	// return { iter: Number, sessionSalt: String }
+}
+
+export async function postSsnAndCallback(data, callback, ...args) {
+	// data = { userId: '', createdAt: 0, sessionPwd: 'abc' }
+	const iter = await getSsnIter(data);
+	docCookies.setItem('userId', data.userId, 1, '/', null, true);
+	docCookies.setItem('createdAt', data.createdAt, 1, '/', null, true);
+	docCookies.setItem('sessionEncrypted', encrypt(iter.sessionSalt, data.sessionPwd, iter.iter), 1, '/', null, true);
+	callback(...args);
+}
+
+export async function getSsns() {
+	const sessions = await instance.get('/account/sessions');
+	return sessions.data;
 }
 
 export async function postLogin(data = { email: 'anonymous@example.com', pwdEncrypted: '123456' }) {
@@ -20,6 +38,12 @@ export async function postLogin(data = { email: 'anonymous@example.com', pwdEncr
 
 export async function postLogout(data = { userId: 'abc', createdAt: 0, sessionEncrypted: 'abc' }) {
 	const result = await instance.post(`/account/log-out`, data);
+	return result.data;
+	// return { message }
+}
+
+export async function postLogoutFromAll(data = { userId: 'abc' }) {
+	const result = await instance.post(`/account/log-out-from-all`, data);
 	return result.data;
 	// return { message }
 }
