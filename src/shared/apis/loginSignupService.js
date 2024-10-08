f9feec7b52bebc93ca2c6b40c2154bdad60ea50e
kipid <kipacti@gmail.com>
2024-10-08 14:06:28 +0900

    Update loginSignupService.js

--- src/shared/apis/loginSignupService.js
+++ src/shared/apis/loginSignupService.js
@@ -6,10 +6,26 @@ export async function postPwdIter(data = { email: '' }) {
 	// return { iter: Number, salt: String }
 }
 
-export async function postSsnIter(data = { userId: '', createdAt: 0 }) {
-	const iter = await instance.post(`/account/session-iter`, data);
+export async function getSsnIter(data = { userId: '', createdAt: 0 }) {
+	docCookies.setItem('userId', data.userId, 1, '/', null);
+	docCookies.setItem('createdAt', data.createdAt, 1, '/', null);
+	const iter = await instance.get(`/account/session-iter`, data);
 	return iter.data;
 	// return { iter: Number, sessionSalt: String }
+}
+
+export async function postSsnAndCallback(data, callback, ...args) {
+	// data = { userId: '', createdAt: 0, sessionPwd: 'abc' }
+	const iter = await getSsnIter(data);
+	docCookies.setItem('userId', data.userId, 1, '/', null);
+	docCookies.setItem('createdAt', data.createdAt, 1, '/', null);
+	docCookies.setItem('sessionEncrypted', encrypt(iter.sessionSalt, data.sessionPwd, iter.iter), 1, '/', null);
+	callback(...args);
+}
+
+export async function getSsns() {
+	const sessions = await instance.get('/account/sessions');
+	return sessions.data;
 }
 
 export async function postLogin(data = { email: 'anonymous@example.com', pwdEncrypted: '123456' }) {
