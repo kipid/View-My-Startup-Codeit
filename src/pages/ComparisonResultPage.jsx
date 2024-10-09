cbc1a108fa6083ee8a455931b767f9f3b1c6e936
Maybeiley <2784519@gmail.com>
2024-10-10 06:37:20 +0900

    Feat: Add sort and rankingList to ComparisonResultPage

--- src/pages/ComparisonResultPage.jsx
+++ src/pages/ComparisonResultPage.jsx
@@ -14,7 +14,7 @@ function MyCompanyBox({ companies, myCompanyId }) {
 	return (
 		<div className={styles.companyInfo}>
 			<img className={styles.companyLogo} src={myCompany.logo ? myCompany.logo : noLogo} alt="로고" />
-			<div className={styles.comanyInfoText}>
+			<div className={styles.companyInfoText}>
 				<p className={styles.companyName}>{myCompany.name}</p>
 				<p className={styles.companyCategory}>{myCompany.category}</p>
 			</div>
@@ -26,7 +26,7 @@ function ResultTable({ myCompanyId, comparisonIds, companies }) {
 	const resultList = [myCompanyId, ...comparisonIds];
 
 	return (
-		<table className={styles.table}>
+		<table className={styles.resultTable}>
 			<tbody>
 				<tr>
 					<th>기업 명</th>
@@ -58,12 +58,56 @@ function ResultTable({ myCompanyId, comparisonIds, companies }) {
 	);
 }
 
+function RankingTable({ myCompanyId, companies }) {
+	const myCompanyIndex = companies.findIndex(company => company.id === myCompanyId);
+	const getRankingList = () => {
+		const start = Math.max(0, myCompanyIndex - 2);
+		const end = Math.min(companies.length, myCompanyIndex + 3);
+		return companies.slice(start, end);
+	};
+	const rankingList = getRankingList();
+
+	return (
+		<table className={styles.rankingTable}>
+			<tbody>
+				<tr>
+					<th>순위</th>
+					<th>기업 명</th>
+					<th>기업 소개</th>
+					<th>카테고리</th>
+					<th>누적 투자 금액</th>
+					<th>매출액</th>
+					<th>고용 인원</th>
+				</tr>
+				{rankingList.map((company, index) => {
+					return (
+						<tr key={company.id}>
+							<td>{myCompanyIndex}위</td>
+							<td className={styles.companyNameCell}>
+								<img className={styles.logo} src={company.logo ? company.logo : noLogo} alt="Company Logo" />
+								&nbsp; {company.name}
+							</td>
+							<td>{company.description}</td>
+							<td>{company.category}</td>
+							<td>{getScaledNumber(company.accumulInvest)}원</td>
+							<td>{getScaledNumber(company.revenue)}원</td>
+							<td>{getScaledNumber(company.employees)}명</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
+}
+
 function ComparisonResultPage() {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [myCompanyId, setMyCompanyId] = useState('');
 	const [comparisonIds, setComparisonIds] = useState([]);
-	const [companies, setCompanies] = useState([]);
-	const [sort, setSort] = useState();
+	const [resultCompanies, setResultCompanies] = useState([]);
+	const [rankingCompanies, setRankingCompanies] = useState([]);
+	const [resultSort, setResultSort] = useState();
+	const [rankingSort, setRankingSort] = useState();
 	const [isPending, errorLoadingCompanies, loadCompaniesAsync, setError] = useAsync(getCompanies);
 
 	const investmentModalHandler = () => {
@@ -83,24 +127,39 @@ function ComparisonResultPage() {
 	}, []);
 
 	useEffect(() => {
-		const fetchCompanies = async () => {
+		const fetchResultCompanies = async () => {
 			try {
-				const result = await loadCompaniesAsync({ take: 100 });
+				const result = await loadCompaniesAsync({ take: 100, sort: resultSort });
 				if (!result) {
-					console.error('No results returned');
 					return;
 				}
-				setCompanies(result.list);
+				setResultCompanies(result.list);
 			} catch (err) {
 				setError(err);
 			}
 		};
 
-		fetchCompanies();
-	}, []);
+		fetchResultCompanies();
+	}, [resultSort]);
+
+	useEffect(() => {
+		const fetchRankingCompanies = async () => {
+			try {
+				const result = await loadCompaniesAsync({ take: 100, sort: rankingSort });
+				if (!result) {
+					return;
+				}
+				setRankingCompanies(result.list);
+			} catch (err) {
+				setError(err);
+			}
+		};
+
+		fetchRankingCompanies();
+	}, [rankingSort]);
 
 	return (
-		companies.length > 0 && (
+		rankingCompanies.length > 0 && (
 			<div>
 				<div className={styles.comparisonResultPage}>
 					<div className={styles.container}>
@@ -113,13 +172,42 @@ function ComparisonResultPage() {
 							</Link>
 						</div>
 						<div className={styles.myCompanyWrapper}>
-							<MyCompanyBox companies={companies} myCompanyId={myCompanyId} />
+							<MyCompanyBox companies={resultCompanies} myCompanyId={myCompanyId} />
 						</div>
 
 						<div className={styles.resultWrapper}>
-							<p className={styles.title}>비교 결과 확인하기</p>
+							<div className={styles.subHeader}>
+								<p className={styles.title}>비교 결과 확인하기</p>
+								<select className={styles.selectOption} value={resultSort} onChange={e => setResultSort(e.target.value)}>
+									<option value="recent">최신순</option>
+									<option value="accumulInvestDesc">누적 투자금액 높은순</option>
+									<option value="accumulInvestAsc">누적 투자금액 낮은순</option>
+									<option value="earningDesc">매출액 높은순</option>
+									<option value="earningAsc">매출액 낮은순</option>
+									<option value="employeeDesc">고용 인원 많은순</option>
+									<option value="employeeAsc">고용 인원 적은순</option>
+								</select>
+							</div>
 							<div className={styles.tableContainer}>
-								<ResultTable myCompanyId={myCompanyId} comparisonIds={comparisonIds} companies={companies} />
+								<ResultTable myCompanyId={myCompanyId} comparisonIds={comparisonIds} companies={resultCompanies} />
+							</div>
+						</div>
+
+						<div className={styles.rankingWrapper}>
+							<div className={styles.subHeader}>
+								<p className={styles.title}>기업 순위 확인하기</p>
+								<select className={styles.selectOption} value={rankingSort} onChange={e => setRankingSort(e.target.value)}>
+									<option value="recent">최신순</option>
+									<option value="accumulInvestDesc">누적 투자금액 높은순</option>
+									<option value="accumulInvestAsc">누적 투자금액 낮은순</option>
+									<option value="earningDesc">매출액 높은순</option>
+									<option value="earningAsc">매출액 낮은순</option>
+									<option value="employeeDesc">고용 인원 많은순</option>
+									<option value="employeeAsc">고용 인원 적은순</option>
+								</select>
+							</div>
+							<div className={styles.tableContainer}>
+								<RankingTable myCompanyId={myCompanyId} companies={rankingCompanies} />
 							</div>
 						</div>
 
