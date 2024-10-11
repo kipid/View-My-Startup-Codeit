ee1df143d9767502b1535a2cddd14aa997faf5b1
Maybeiley <2784519@gmail.com>
2024-10-11 17:16:11 +0900

    Feat: Add complete modal after Update/Delete investment

--- src/components/InvestmentUpdateModal.jsx
+++ src/components/InvestmentUpdateModal.jsx
@@ -5,6 +5,8 @@ import noImage from '../assets/no_image.png';
 import eyeOn from '../assets/ic_eye_on.png';
 import eyeOff from '../assets/ic_eye_off.png';
 import { updateInvestment } from '../shared/apis/investmentApis.js';
+import Modal from './Modal.jsx';
+import InvestmentCompleteModal from './InvestmentCompleteModal.jsx';
 
 const initialValidation = {
 	isAmountOk: false,
@@ -17,6 +19,7 @@ function InvestmentUpdateModal({ investmentDetail, companyDetail, onClose, onUpd
 	const [detail, setDetail] = useState(investmentDetail);
 	const [pw, setPw] = useState('');
 	const [validation, setValidation] = useState(initialValidation);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	// NOTE show가 false이면 아무것도 렌더하지 않음
 	if (!show) return null;
@@ -48,97 +51,107 @@ function InvestmentUpdateModal({ investmentDetail, companyDetail, onClose, onUpd
 			const investment = await updateInvestment(id, body);
 		};
 		updateData();
-		onUpdate();
+		setIsModalOpen(true);
 	};
 
 	return (
 		<div id={style.InvestmentUpdateModal}>
-			<div id={style.modalHeader}>
-				<p>투자 정보 수정</p>
-				<button type="button" onClick={onClose}>
-					<img src={icDelete} alt="modalOff" />
-				</button>
-			</div>
-
-			<form id={style.modalBody}>
-				<div id={style.info}>
-					<p>투자 기업 정보</p>
-					<div>
-						<img src={companyDetail.logo ?? noImage} alt="companyLogo" />
-						<p>{companyDetail.name}</p>
-						<p>{companyDetail.category}</p>
+			{isModalOpen ? (
+				<Modal>
+					<InvestmentCompleteModal onClose={() => onUpdate()} show />
+				</Modal>
+			) : (
+				<>
+					<div id={style.modalHeader}>
+						<p>투자 정보 수정</p>
+						<button type="button" onClick={onClose}>
+							<img src={icDelete} alt="modalOff" />
+						</button>
 					</div>
-				</div>
-				<div id={style.name}>
-					<label htmlFor="name">투자자 이름</label>
-					<input
-						id="name"
-						type="text"
-						placeholder="투자자 이름을 입력해 주세요"
-						value={detail.name}
-						onChange={e =>
-							setDetail(prev => {
-								return { ...prev, name: e.target.value };
-							})
-						}
-					/>
-				</div>
-				<div id={style.amount}>
-					<label htmlFor="amount">
-						투자 금액{' '}
-						{!validation.isFirst && !validation.isAmountOk && <span className={style.errorMsg}>숫자로 입력해주세요.</span>}
-					</label>
-					<input
-						id="amount"
-						type="number"
-						placeholder="투자 금액을 입력해 주세요"
-						value={detail.amount}
-						onChange={e =>
-							setDetail(prev => {
-								return { ...prev, amount: e.target.value };
-							})
-						}
-					/>
-				</div>
-				<div id={style.comment}>
-					<label htmlFor="comment">투자 코멘트</label>
-					<textarea
-						id="comment"
-						placeholder="투자에 대한 코멘트를 입력해 주세요"
-						value={detail.comment}
-						onChange={e =>
-							setDetail(prev => {
-								return { ...prev, comment: e.target.value };
-							})
-						}
-					/>
-				</div>
-				<div id={style.password}>
-					<label htmlFor="password">
-						비밀번호{' '}
-						{!validation.isFirst && !validation.isPasswordOk && <span className={style.errorMsg}>비밀번호를 입력해주세요.</span>}
-					</label>
-					<input
-						id="password"
-						type={isPWshow ? '' : 'password'}
-						placeholder="비밀번호를 입력해 주세요"
-						value={pw}
-						onChange={e => {
-							setPw(e.target.value);
-						}}
-					/>
-					<img id={style.eye} src={isPWshow ? eyeOn : eyeOff} alt="비밀번호 표시" onClick={togglePWshow} />
-				</div>
-			</form>
 
-			<div id={style.modalFooter}>
-				<button type="button" className={`${style.modal} ${style.cancel}`} onClick={onClose}>
-					취소
-				</button>
-				<button type="button" className={style.modal} onClick={handleUpdate}>
-					수정하기
-				</button>
-			</div>
+					<form id={style.modalBody}>
+						<div id={style.info}>
+							<p>투자 기업 정보</p>
+							<div>
+								<img src={companyDetail.logo ?? noImage} alt="companyLogo" />
+								<p>{companyDetail.name}</p>
+								<p>{companyDetail.category}</p>
+							</div>
+						</div>
+						<div id={style.name}>
+							<label htmlFor="name">투자자 이름</label>
+							<input
+								id="name"
+								type="text"
+								placeholder="투자자 이름을 입력해 주세요"
+								value={detail.name}
+								onChange={e =>
+									setDetail(prev => {
+										return { ...prev, name: e.target.value };
+									})
+								}
+							/>
+						</div>
+						<div id={style.amount}>
+							<label htmlFor="amount">
+								투자 금액{' '}
+								{!validation.isFirst && !validation.isAmountOk && <span className={style.errorMsg}>숫자로 입력해주세요.</span>}
+							</label>
+							<input
+								id="amount"
+								type="number"
+								placeholder="투자 금액을 입력해 주세요"
+								value={detail.amount}
+								onChange={e =>
+									setDetail(prev => {
+										return { ...prev, amount: e.target.value };
+									})
+								}
+							/>
+						</div>
+						<div id={style.comment}>
+							<label htmlFor="comment">투자 코멘트</label>
+							<textarea
+								id="comment"
+								placeholder="투자에 대한 코멘트를 입력해 주세요"
+								value={detail.comment}
+								onChange={e =>
+									setDetail(prev => {
+										return { ...prev, comment: e.target.value };
+									})
+								}
+							/>
+						</div>
+						<div id={style.password}>
+							<label htmlFor="password">
+								비밀번호{' '}
+								{!validation.isFirst && !validation.isPasswordOk && (
+									<span className={style.errorMsg}>비밀번호를 입력해주세요.</span>
+								)}
+							</label>
+							<input
+								id="password"
+								type={isPWshow ? '' : 'password'}
+								placeholder="비밀번호를 입력해 주세요"
+								value={pw}
+								onChange={e => {
+									setPw(e.target.value);
+								}}
+							/>
+							<img id={style.eye} src={isPWshow ? eyeOn : eyeOff} alt="비밀번호 표시" onClick={togglePWshow} />
+						</div>
+					</form>
+
+					<div id={style.modalFooter}>
+						<button type="button" className={`${style.modal} ${style.cancel}`} onClick={onClose}>
+							취소
+						</button>
+						<button type="button" className={style.modal} onClick={handleUpdate}>
+							수정하기
+						</button>
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
